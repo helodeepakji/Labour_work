cd7e5d1aef34e3f71444a4cd87d6697ea132d171
helodeepakji <helodeepakji@gmail.com>
2023-06-01 21:26:35 +0530

    frontend code

--- api/views.py
+++ api/views.py
@@ -1,3 +1,16 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
+def signin(request):
+    print(request.POST)
+    return JsonResponse({'message': 'Form submitted successfully'})
+
+
+def signup(request):
+    print(request.POST)
+    return JsonResponse({'message': 'Form submitted successfully'})
+
+def contact(request):
+    print(request.POST)
+    return JsonResponse({'message': 'Form submitted successfully'})
